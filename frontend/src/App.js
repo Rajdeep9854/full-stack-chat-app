01c896d4e2a33b52748cf9b26af86697f428892a
Rajdeep <rajdeep98543231chowdhury@gmail.com>
2024-04-19 11:22:05 +0530

    creating frontend with chakra ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
-
+import { Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ChatPage from "./pages/ChatPage";
 
 function App() {
   return (
     <div className="App">
-     Rajdeep
+     
+      <Route path="/" component={HomePage} exact></Route>
+      <Route  path="/chats" component={ChatPage} />
     </div>
   );
 }
