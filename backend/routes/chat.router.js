ed9e19a37d16e03b6ac45bff1cadf919e12b3777
Rajdeep <rajdeep98543231chowdhury@gmail.com>
2024-04-27 11:20:55 +0530

    create new Group api and rename new Group api

--- backend/routes/chat.router.js
+++ backend/routes/chat.router.js
@@ -1,13 +1,17 @@
 
 const { protect } = require('../middleware/authMiddleware.js')
-const {accessChat,fetchChats} = require('../controllers/chat.controller.js')
+const { accessChat
+    ,fetchChats
+    , createGroup,
+    renameGroup} = require('../controllers/chat.controller.js')
 
 const express = require('express')
 const router = express.Router();
 
 router.route('/').post(protect, accessChat)
 router.route('/').get(protect,fetchChats)
-
+router.route('/group').post(protect, createGroup)
+router.route('/group/rename').put(protect,renameGroup)
 
 
 module.exports = router
